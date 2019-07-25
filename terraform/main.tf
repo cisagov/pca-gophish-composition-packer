e900dad0f18ba72d6ee7e5b31f73cd65184e7310
Dave Redmin <david.redmin@beta.dhs.gov>
2019-07-25 00:08:57 -0400

    Install a VNC server

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "iam_user" {
   source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-module"
 
   add_packer_permissions = true
-  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*"]
+  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*", "/vnc/username", "/vnc/password"]
   user_name              = "test-pca-gophish-composition-packer"
   tags = {
     Team        = "CISA - Development"
