198b4db3fe0b200f49168f801932a5c4c04f5cdb
Dave Redmin <david.redmin@beta.dhs.gov>
2019-08-11 16:26:03 -0400

    Update terraform to allow Travis CI test user to access (in SSM) SSH keypair for VNC user

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "iam_user" {
   source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-module"
 
   add_packer_permissions = true
-  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*", "/vnc/username", "/vnc/password"]
+  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*", "/vnc/username", "/vnc/password", "/vnc/ssh/rsa_public_key", "/vnc/ssh/rsa_private_key"]
   user_name              = "test-pca-gophish-composition-packer"
   tags = {
     Team        = "CISA - Development"
